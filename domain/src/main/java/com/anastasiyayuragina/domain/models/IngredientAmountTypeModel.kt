cb43bd524dc09085496f4bfa2428a51277073e9d
AnastasiyaYuragina <dark25555@gmail.com>
2022-06-03 23:57:25 +0300

    add recipe models

---
+++ domain/src/main/java/com/anastasiyayuragina/domain/models/IngredientAmountTypeModel.kt
@@ -0,0 +1,9 @@
+package com.anastasiyayuragina.domain.models
+
+enum class IngredientAmountTypeModel(id: Int) {
+    Grams(0),
+    Tablespoon(1),
+    TeaSpoon(2),
+    Pieces(3),
+    Glass(4);
+}
---
+++ domain/src/main/java/com/anastasiyayuragina/domain/models/RecipeModel.kt
@@ -0,0 +1,14 @@
+package com.anastasiyayuragina.domain.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RecipeModel(
+    val id: Int,
+    val name: String,
+    val image: String? = null,
+    val description: String? = null,
+    val recipeUrl: String? = null,
+    val ingredientModels: List<IngredientModel>? = null
+)
+
